ea8dc8bec2412fb82079513b88ec841b58997f77
Tamutnih <renu07@list.ru>
2023-06-07 23:13:15 +0500

    new folder

---
+++ Program/Database/db_config.py
@@ -0,0 +1,12 @@
+from getpass import getpass
+
+
+def config_db_init():
+    host = 'localhost'
+    user = input('Enter the database server login: ')
+    password = getpass('Enter the database server password: ')
+    database = 'Human_friends'
+    return [host, user, password, database]
+
+
+db_connect.py
